437c4df9ad5ec2d65d33dbfe4d3c86c4a4fce7a3
hont-dev_lhy <hywkd3@gmail.com>
2022-11-28 00:50:01 +0900

    수정: 기존 App 코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    const [text, setText] = useState<string>('text');
-    // useEffect(() => {
-    //   fetch('https://jsonplaceholder.typicode.com/users')
-    //     .then((response) => response.json())
-    //     .then((json) => setUser(json));
-    // }, []);
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-                <a>{text}</a>
-            </header>
-        </div>
-    );
+    return <CartContainer></CartContainer>;
 }
 
 export default App;
